e04c67a5fc593b3c257c3758b0f4478534ae2c7e
atroopa <atroopa@gmail.com>
2023-07-02 14:02:24 +0330

    first commit

---
+++ function.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x int
+	var y int
+
+	x = 12
+	y = 25
+
+	fmt.Println("x is %v and typ of is %T", x, x)
+	fmt.Println("y is %v and typ of is %t", y, y)
+
+}
