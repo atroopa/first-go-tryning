9bf86497649bd959bcef2270ff16e0782421eee9
atroopa <atroopa@gmail.com>
2023-07-02 14:12:53 +0330

    can do not use declering in go

--- function.go
+++ function.go
@@ -3,18 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var x float64
-	var y float64
+	// var x float64
+	// var y float64
 
-	x = 12
-	y = 25
+	x := 12.0
+	y := 25.0
 
 	fmt.Printf("x is %v and typ of is %T \n", x, x)
 	fmt.Printf("y is %v and typ of is %T \n", y, y)
 
 	var mean float64
 
-	mean = (x + y) / 2.3
+	mean = (x + y) / 2.4
 
 	fmt.Printf("mean result is : %v and type: %T \n", mean, mean)
 }
