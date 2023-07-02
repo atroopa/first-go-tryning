09276910e1a1c43236ba4f5122be7c7eb5b3b34f
atroopa <atroopa@gmail.com>
2023-07-02 14:10:46 +0330

    now know about float64 type

--- function.go
+++ function.go
@@ -3,13 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var x int
-	var y int
+	var x float64
+	var y float64
 
 	x = 12
 	y = 25
 
-	fmt.Println("x is %v and typ of is %T", x, x)
-	fmt.Println("y is %v and typ of is %t", y, y)
+	fmt.Printf("x is %v and typ of is %T \n", x, x)
+	fmt.Printf("y is %v and typ of is %T \n", y, y)
 
+	var mean float64
+
+	mean = (x + y) / 2.3
+
+	fmt.Printf("mean result is : %v and type: %T \n", mean, mean)
 }
